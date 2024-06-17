92d357e608502b3375e4fadd4d66355f9c0c9642
ishantSikdar <ishant9715@gmail.com>
2024-06-17 16:31:36 +0530

    Re-engineered ChatMessage component to include multiple messages

--- src/components/chat/ChatMessage.jsx
+++ src/components/chat/ChatMessage.jsx
@@ -1,16 +1,26 @@
-export default function ChatMessage({ userId, message }) {
+export default function ChatMessage({ messageComponents }) {
+  const messageComponent = {
+    name: "Ishant",
+    profileUrl: "/photo.jpg",
+    time: "17/06/24 - 16:26",
+    messages: [
+      "I am so done",
+      "with this game", 
+    ],
+  };
+
   return (
     <div className="w-full flex gap-4 px-4 mt-4">
       <div className="rounded-full h-6 p-6" style={{
-        backgroundImage: 'url("/photo.jpg")',
+        backgroundImage: `url(${messageComponent.profileUrl})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover'
       }}></div>
       <div>
-        <p className="text-green-500 font-medium">Ishant <span className="text-white font-normal text-xs ml-3">Yesterday at 9:07</span></p>
-        <p className="text-frostWhite ">
-          Hi, I am a chat message
-        </p>
+        <p className="text-green-500 font-medium">{messageComponent.name}<span className="text-white font-normal text-xs ml-3">{messageComponent.time}</span></p>
+        <div className="text-frostWhite font-thin">
+          {messageComponent.messages.map((message, idx) => <p key={idx}>{message}</p>)}
+        </div>
       </div>
     </div>
   )
