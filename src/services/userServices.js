0f1a44ae3a5888fdff52821c0db495b041cbd7d4
ishantSikdar <ishant9715@gmail.com>
2024-06-18 17:59:29 +0530

    Fixed Multipart formData bug

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -15,19 +15,19 @@ export const sendLoginRequest = async (loginForm) => {
 export const sendSignUpRequest = async (signUpForm) => {
     try {
         const formData = new FormData();
-        formData.append('image', signUpForm.image);
         formData.append('json', JSON.stringify(JSON.stringify({
-            email: signUpForm.email,
             name: signUpForm.name,
             username: signUpForm.username,
-            mobile: signUpForm.mobile,
+            email: signUpForm.email,
             password: signUpForm.password,
+            mobile: signUpForm.mobile,
         })));
-        return await postMultipartFormDataRequest(`${API_BASE_URL}${API_URI_USER_SIGNUP}`, signUpForm);
+        formData.append('image', signUpForm.image);
+        return await postMultipartFormDataRequest(`${API_BASE_URL}${API_URI_USER_SIGNUP}`, formData);
 
     } catch (error) {
-        console.error(`Login request failed, ${error.message}`);
-        throw new Error(`Login request failed, ${error.message}`);
+        console.error(`Signup request failed, ${error.message}`);
+        throw new Error(`Signup request failed, ${error.message}`);
     }
 };
 
@@ -54,7 +54,7 @@ export const sendEditUserDetailsRequest = async (userDetails) => {
             email: userDetails.email,
         })))
         return await putMultipartFormDataAuthenticatedRequest(`${API_BASE_URL}${API_URI_USER_DETAILS}`, formData, localToken);
-    
+
     } catch (error) {
         console.error(`Edit User Details request failed, ${error.message}`);
         throw new Error(`${error.message}`);
