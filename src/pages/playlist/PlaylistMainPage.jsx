9e9d797697b074b3ec8b39d8554db571f42f9b73
ishantSikdar <ishant9715@gmail.com>
2024-06-21 01:14:37 +0530

    Add Debouncing Effect for Search Bar - Playlist Page

--- src/pages/playlist/PlaylistMainPage.jsx
+++ src/pages/playlist/PlaylistMainPage.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PlaylistContent from "../../components/playlist/PlaylistContent"
 
 export default function PlaylistMainPage() {
   const [searchTag, setSearchTag] = useState("");
+  const [searchDebounced, setSearchDebounced] = useState("");
 
   const handleSearchTagInput = (event) => {
     setSearchTag(event.target.value);
   }
 
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      setSearchDebounced(searchTag);
+    }, 800)
+
+    return () => {
+      clearInterval(debounceTimer);
+    }
+  }, [searchTag]);
+
   return (
-    <div className="relative flex flex-col items-center gap-5 px-2 pt-36 py-10">
-      <input type="text" name="searchTag" placeholder="Try typing a subject..." className="fixed top-14 outline-none  bg-light-gray py-4 px-6 rounded-b-3xl w-full" onChange={handleSearchTagInput} />
-      <PlaylistContent search={searchTag} />
+    <div className="relative flex flex-col items-center gap-5 px-2 pt-20 py-10">
+      <input type="text" name="searchTag" placeholder="Try typing a subject..." className="fixed text-sm top-0 outline-none bg-gray py-4 px-6 w-full" onChange={handleSearchTagInput} />
+      <PlaylistContent search={searchDebounced} />
     </div>
   )
 }
